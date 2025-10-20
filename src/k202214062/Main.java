4a3f69e847ad4f5c547553f62c25ed0b2e5a11a6
Lee, juhyung <p99265407@gmail.com>
2025-10-20 16:13:27 +0900

    merge conflict

--- src/k202214062/Main.java
+++ src/k202214062/Main.java
@@ -2,7 +2,9 @@ package k202214062;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("이주형");
+		System.out.println("홍길동");
+		System.out.println("안녕하세요");
+		World.run();
 		Hello.run();
 	}
 }
